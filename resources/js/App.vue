28402980814f14d339895245a83575d2f92ad43c
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-02-02 15:40:55 +0100

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <div class="container">
+            <h1>Homepage Frontoffice on Vue</h1>
+
+            <ol>
+                <li v-for="post in arrPosts" :key="post.id">{{ post.title }}</li>
+            </ol>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+        arrPosts: [],
+    };
+  },
+  created() {
+    axios.get('/api/posts')
+      .then(response => this.arrPosts = response.data.results);
+  }
+}
+</script>
+
+<style lang="scss">
+    @import '~bootstrap/scss/bootstrap';
+</style>
+
